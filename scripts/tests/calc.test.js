9c9853be57b7ecff6f3ce21d17e2135c9e1a61bf
Maurizio <maurizio.loffredo@outlook.com>
2023-01-02 21:54:22 +0000

    Initial commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,20 @@
+// 'npm init' command in Terminal has been run and 'jest' has been specified as "test command"
+
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
